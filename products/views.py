a40d454b886638b0512a91f5fef31432a37d20c0
Henna Singh <hennasingh04@gmail.com>
2025-03-30 13:53:44 +0100

    Add sorting by price, rating and category on the products page #17

--- products/views.py
+++ products/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect, reverse, get_object_or_404
 from django.contrib import messages
 from django.db.models import Q
 from .models import Product, Category
+from django.db.models.functions import Lower
 
 # Create your views here.
 
@@ -55,8 +56,23 @@ def all_products(request):
     playtime = None
     playercount = None
     age = None
+    sort = None
+    direction = None
 
     if request.GET:
+        if 'sort' in request.GET:
+            sortkey = request.GET['sort']
+            sort = sortkey
+            if sortkey == 'name':
+                sortkey = 'lower_name'
+                products = products.annotate(lower_name=Lower('name'))
+
+            if 'direction' in request.GET:
+                direction = request.GET['direction']
+                if direction == 'desc':
+                    sortkey = f'-{sortkey}'
+            products = products.order_by(sortkey)
+
         if 'age' in request.GET:
             age = request.GET['age']
             min_age, max_age = get_age_range(age)
@@ -90,16 +106,19 @@ def all_products(request):
             if not query:
                 messages.error(request, "You didnt enter any search criteria!")
                 return redirect(reverse('products'))
-  
+
             queries = (
                 Q(name__icontains=query) | Q(description__icontains=query)
             )
             products = products.filter(queries)
 
+    current_sorting = f'{sort}_{direction}'
+
     context = {
         'products': products,
         'search_term': query,
         'current_categories': categories,
+        'current_sorting': current_sorting,
     }
 
     return render(request, 'products/products.html', context)
