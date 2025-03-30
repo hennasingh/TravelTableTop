5d7da094a9aa891e485d9b1b1eb9137d96e765c0
Henna Singh <hennasingh04@gmail.com>
2025-03-30 10:50:18 +0100

    Add category, age, player count, playtime filtering #17

--- products/views.py
+++ products/views.py
@@ -1,9 +1,48 @@
 from django.shortcuts import render, redirect, reverse, get_object_or_404
 from django.contrib import messages
 from django.db.models import Q
-from .models import Product
+from .models import Product, Category
 
 # Create your views here.
+
+
+def get_playtime_range(playtime_filter):
+    """
+    Returns a tuple of playtime range based on the filter
+    """
+    ranges = {
+        'quick': (0, 30),
+        'medium': (31, 60),
+        'long': (61, 120),
+    }
+    return ranges.get(playtime_filter, (None, None))
+
+
+def get_player_count_range(player_count_filter):
+    """
+    Returns a tuple of player count range based on the filter
+    """
+    ranges = {
+        'solo': (1, 1),
+        'duo': (2, 2),
+        'trio': (3, 6),
+        'penta': (5, 99),
+    }
+
+    return ranges.get(player_count_filter, (0, 99))
+
+
+def get_age_range(age_filter):
+    """
+    Returns a tuple of age range based on the filter
+    """
+    ranges = {
+        'kids': (0, 7),
+        'children': (8, 12),
+        'teens': (13, 17),
+        'adult': (18, 99),
+    }
+    return ranges.get(age_filter, (0, 99))
 
 
 def all_products(request):
@@ -12,14 +51,46 @@ def all_products(request):
     """
     products = Product.objects.all()
     query = None
+    categories = None
+    playtime = None
+    playercount = None
+    age = None
 
     if request.GET:
+        if 'age' in request.GET:
+            age = request.GET['age']
+            min_age, max_age = get_age_range(age)
+            products = products.filter(
+                min_age__gte=min_age, min_age__lte=max_age
+            )
+
+        if 'playercount' in request.GET:
+            playercount = request.GET['playercount']
+            min_playercount, max_playercount = get_player_count_range(
+                playercount)
+            products = products.filter(
+                Q(min_players__lte=max_playercount) &
+                Q(max_players__gte=min_playercount)
+            )
+
+        if 'playtime' in request.GET:
+            playtime = request.GET['playtime']
+            min_playtime, max_playtime = get_playtime_range(playtime)
+            products = products.filter(
+                playing_time__gte=min_playtime, playing_time__lte=max_playtime
+            )
+
+        if 'category' in request.GET:
+            categories = request.GET['category'].split(',')
+            products = products.filter(category__name__in=categories)
+            categories = Category.objects.filter(name__in=categories)
+
         if 'q' in request.GET:
             query = request.GET['q']
             if not query:
                 messages.error(request, "You didnt enter any search criteria!")
                 return redirect(reverse('products'))
-
+  
             queries = (
                 Q(name__icontains=query) | Q(description__icontains=query)
             )
@@ -28,6 +99,7 @@ def all_products(request):
     context = {
         'products': products,
         'search_term': query,
+        'current_categories': categories,
     }
 
     return render(request, 'products/products.html', context)
