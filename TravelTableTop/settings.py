d09b6824f73b2b11234fd6c07168239dfc2f37b1
Henna Singh <hennasingh04@gmail.com>
2025-04-01 14:12:57 +0100

    Configure deployment, add env file, postgres, procfile, runtime and secret keys.

--- TravelTableTop/settings.py
+++ TravelTableTop/settings.py
@@ -12,6 +12,9 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 import os
+import dj_database_url
+if os.path.isfile('env.py'):
+    import env
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -21,12 +24,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-#^i1)wkcwn9njs%9)(#*s4b3bu(%5axpyjjb(6m3l638o@hmul'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = True if os.path.isfile('env.py') else False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['travel-table-top-6a0c25007929.herokuapp.com/']
 
 
 # Application definition
@@ -106,13 +109,17 @@ WSGI_APPLICATION = 'TravelTableTop.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+if 'DATABASE_URL' in os.environ:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
-}
-
+else: 
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
