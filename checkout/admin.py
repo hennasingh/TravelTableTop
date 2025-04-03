7d4ac64568e11e01d0bd0be6a582e2f3fdced2e3
Henna Singh <hennasingh04@gmail.com>
2025-04-03 09:23:30 +0100

    Completed webhook handler with checks for secure payment and orders #27

--- checkout/admin.py
+++ checkout/admin.py
@@ -20,7 +20,8 @@ class OrderAdmin(admin.ModelAdmin):
     inlines = (OrderLineItemAdminInline,)
 
     readonly_fields = ('order_number', 'date', 'order_total',
-                       'delivery_cost', 'grand_total')
+                       'delivery_cost', 'grand_total', 'original_bag',
+                       'stripe_pid')
 
     fields = (
         'order_number',
@@ -36,7 +37,9 @@ class OrderAdmin(admin.ModelAdmin):
         'county',
         'delivery_cost',
         'order_total',
-        'grand_total'
+        'grand_total',
+        'original_bag',
+        'stripe_pid',
     )
     list_display = (
         'order_number',
